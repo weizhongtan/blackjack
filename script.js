eb339558a3a7d776206cf62a5c59bb7013f43d6a
Wei-Zhong Tan <wztan@hotmail.co.uk>
2016-12-07 18:55:40 +0000

    Added Card Sprite animations

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 /*
 52 cards
 aim is to get as close to 21 as possible
@@ -7,20 +9,56 @@ dealer and player are dealt two random cards
 player can choose to hit (take another card) or
 */
 
+/*
+current issues: when you go bust it breaks
+*/
+
 // GAME SETTINGS
 var BET_INCREMENT = 100;
 var STARTING_MONEY = 1000;
 
+// deck must be global since it changes throughout the game
+var _deck, playerHand, dealerHand, playerbust = false, dealerbust = false, dealersTurn = false, result, bettingover = false, betamount, cash = STARTING_MONEY;
+
+// keeps track of how many cards are currently showing to the player
+var _cardsShownPlayer;
+var _cardsShownDealer;
+
+// target dom by ID
+function $id(id) {
+	return document.getElementById(id)
+}
+
 // init
 function init() {
-	document.getElementById("hit").disabled = true;
-	document.getElementById("stay").disabled = true;
+	$id("hit").disabled = true;
+	$id("stay").disabled = true;
 	startBets();
+	_deck = generateDeck();
+	generateCardSprites(_deck);
+	_cardsShownPlayer = 0;
+	_cardsShownDealer = 0;
+}
+
+// generates card sprites from sprite gif file
+function generateCardSprites(d) {
+	var count = 0;
+	for (var suit = 0; suit < 4; suit++) {
+		for (var val = 0; val < 13; val++) {
+			var style = document.createElement("style");
+			style.type = "text/css";
+			style.innerHTML = ".card_" + d[count] + " { background-position: " + -81*val + "px " + -117.5*suit + "px; }";
+			document.getElementsByTagName("head")[0].appendChild(style);
+			count++;
+		}
+	}
 }
 
 function startBets() {
-	betamount = BET_INCREMENT;
-	cash -= betamount;
+	if (cash !== 0) {
+		betamount = BET_INCREMENT;
+		cash -= betamount;
+	}
 	bettingover = false;
 	playerbust = false;
 	dealerbust = false;
@@ -28,16 +66,34 @@ function startBets() {
 	renderBets();
 }
 
+// generate inital full deck of 52 cards
+function generateDeck() {
+	var deck = []
+	var suits = ["h", "d", "c", "s"]
+	// one for each suit (hearts, diamonds, clubs, spades)
+	for (var i = 0; i < 4; i++) {
+		// current suit
+		var s = suits[i];
+		// cards 2-9
+		for (var j = 2; j <= 9; j++) {
+			deck.push(s+j)
+		}
+		// cards 10, j, q, k, a
+		deck.push(s+"T", s+"J", s+"Q", s+"K", s+"A");
+	}
+	return deck
+}
+
 function button_play() {
 	// disable betting area
 	bettingover = true;
 	renderBets();
 
-	deck = generateDeck();
+	_deck = generateDeck();
 
 	// inital hands
-	playerHand = generateHand(deck);
-	dealerHand = generateHand(deck);
+	playerHand = generateHand(_deck);
+	dealerHand = [randomCard(_deck)];
 
 	// FOR TESTING PURPOSES
 	// playerHand = [[10], ["ACE"]]
@@ -46,47 +102,50 @@ function button_play() {
 	// render html
 	render()
 
-	// populate deck
-	function generateDeck() {
-		var deck = []
-		// one for each suit
-		for (var i = 0; i < 4; i++) {
-			// cards 2-10
-			for (var j = 2; j <= 10; j++) {
-				deck.push(j)
-			}
-			// cards ace j, q, k
-			deck.push("ACE", "J", "Q", "K");
-		}
-		return deck
-	}
-
-	// generate initial 2 hand
-	function generateHand() {
+	// generate initial 2 card hand
+	function generateHand(deck) {
 		return [randomCard(deck), randomCard(deck)]
 	}
 }
 
 function render() {
-	// render html
-	document.getElementById("player").innerHTML = playerHand.toString() + " (sum: " + renderSum(playerHand) + ")";
-	document.getElementById("playerbust").innerHTML = (playerbust) ? " -> BUST!" : "";
-	document.getElementById("dealer").innerHTML = dealerHand[0].toString();
-	document.getElementById("dealerbust").innerHTML = (dealerbust) ? " -> BUST!" : "";
+	// render card sprites of given hand
+	function showCards(person, hand, cards_shown) {
+		if (cards_shown == 0) {
+			$id(person).innerHTML = "";
+		}
+		for (; cards_shown < hand.length; cards_shown++) {
+			var div = document.createElement("div");
+			div.className = "card card_" + hand[cards_shown];
+			$id(person).appendChild(div);
+		}
+		if (person == "player") {
+			_cardsShownPlayer = cards_shown;
+		} else if (person == "dealer") {
+			_cardsShownDealer = cards_shown;
+		}
+		$id(person +"sum").innerHTML = "(sum: " + renderSum(hand) + ")";
+	}
+
+	showCards("player", playerHand, _cardsShownPlayer);
+
+	$id("playerbust").innerHTML = (playerbust) ? " -> BUST!" : "";
+	showCards("dealer", dealerHand, _cardsShownDealer);
+	$id("dealerbust").innerHTML = (dealerbust) ? " -> BUST!" : "";
 
 	// disable/enable buttons
-	document.getElementById("hit").disabled = playerbust;
-	document.getElementById("stay").disabled = playerbust;
+	$id("hit").disabled = playerbust;
+	$id("stay").disabled = playerbust;
 
 	if (dealersTurn) {
-		document.getElementById("dealer").innerHTML = dealerHand.toString() + " (sum: " + renderSum(dealerHand) + ")";
-		document.getElementById("result").innerHTML = "you " + result;
+		showCards("dealer", dealerHand, _cardsShownDealer);
+		$id("result").innerHTML = "you " + result;
 
 		// disable/enable buttons
-		document.getElementById("hit").disabled = true;
-		document.getElementById("stay").disabled = true;
+		$id("hit").disabled = true;
+		$id("stay").disabled = true;
 	} else {
-		document.getElementById("result").innerHTML = "";
+		$id("result").innerHTML = "";
 	}
 
 	function renderSum(playerHand) {
@@ -99,62 +158,68 @@ function render() {
 }
 
 function renderBets() {
-	document.getElementById("cash").innerHTML = cash.toString();
+	$id("cash").innerHTML = cash.toString();
 
-	document.getElementById("bet").innerHTML = betamount.toString();
-	document.getElementById("bet").disabled = bettingover;
-	document.getElementById("play").disabled = bettingover;
+	$id("bet").innerHTML = betamount.toString();
+	$id("bet").disabled = bettingover;
+	$id("play").disabled = bettingover;
 }
 
 // returns random card from deck and removes it from the deck
-function randomCard() {
+function randomCard(deck) {
 	var randNum = Math.floor(Math.random()*deck.length);
 	return deck.splice(randNum, 1)
 }
 
 // play buttons
 function button_hit() {
-	playerHand.push(randomCard());
+	playerHand.push(randomCard(_deck));
 	playerbust = checkIfBust(playerHand);
 	bettingover = true;
 	highlight("player");
 	render();
+
+	if (playerbust) {
+		init()
+	}
 }
 
 function button_stay() {
 	dealersTurn = true;
 	while (bestSum(dealerHand) < 17) {
-		dealerHand.push(randomCard());
+		dealerHand.push(randomCard(_deck));
 	}
 	dealerbust = checkIfBust(dealerHand);
 	result = whoWins();
 	highlight("dealer");
 	render();
 
-	startBets();
+	init();
 }
 
+// returns an array of the highest and lowest possible sums of a given hand
 function calculateSum(hand) {
 	var sum1 = 0;
 	var sum2 = 0;
 	for (var i = 0; i < hand.length; i++) {
 		var card = hand[i][0];
-		if (typeof card == "string") {
-			if (card == "J" || card == "Q" || card == "K") {
+		var card_suit = card.charAt(0);
+		var card_value = card.charAt(1);
+		if (card_value == "T" || card_value == "J" || card_value == "Q" || card_value == "K") {
 				sum1 += 10;
 				sum2 += 10;
-			} else {
+		} else if (card_value == "A") {
 				sum1 += 1;
 				sum2 += 11;
-			}
 		} else {
-			sum1 += card;
-			sum2 += card;
+			sum1 += +card_value;
+			sum2 += +card_value;
 		}
 	}
 	return [sum1, sum2];
 }
 
+// returns the best value of a given hand
 function bestSum(hand) {
 	var sums = calculateSum(hand);
 	if (sums[1] > sums[0] && sums[1] <= 21) {
@@ -193,20 +258,20 @@ function whoWins() {
 
 // betting
 function button_bet() {
-	betamount += BET_INCREMENT;
-	cash -= BET_INCREMENT;
+	if (cash !== 0) {
+		cash -= BET_INCREMENT;
+		betamount += BET_INCREMENT;
+	}
 	renderBets();
 	highlight("cash");
 }
 
 function highlight(cls) {
-	document.getElementById(cls).className = "highlighted";
+	$id(cls).className = "highlighted";
 	setTimeout(function() {
-		document.getElementById(cls).className = "";
+		$id(cls).className = "";
 	}, 500)
 }
-
-var deck, playerHand, dealerHand, playerbust = false, dealerbust = false, dealersTurn = false, result, bettingover = false, betamount, cash = STARTING_MONEY;
 
 init();
 
